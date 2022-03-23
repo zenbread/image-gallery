5fe01ec01af88a5988dad9aa434af52588199e9e
Nick Camp <nmcamp87@gmail.com>
2022-03-23 11:48:11 -0700

    Added api"

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
-
-
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+const UNSPLASH_API = "https://api.unsplash.com/"
 
 function App() {
   const [word, setWord] = useState('');
   function handleSearchSubmit(e) {
     e.preventDefault();
     console.log(word);
+    fetch(`${UNSPLASH_API}photos/random?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   }
 
   return (
